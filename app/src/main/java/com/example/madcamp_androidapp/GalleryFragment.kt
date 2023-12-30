ab87958219ce6c0a4dd6358c8cffdba70caef0e4
Kim Sunoh <sunoh1222@gmail.com>
2023-12-30 10:19:48 +0900

    fix: 갤러리 구성 레이아웃 수정
    갤러리를 구성하는 레이아웃을 그리드 레이아웃 대신 세 개의 recycler view를 묶은 리니어 레이아웃으로 대체했습니다. 가로 길이가 같으면서 이미지 비율에 맞게 세로 길이가 다르게 출력되는 것이 가능합니다.

--- app/src/main/java/com/example/madcamp_androidapp/GalleryFragment.kt
+++ app/src/main/java/com/example/madcamp_androidapp/GalleryFragment.kt
@@ -1,18 +1,13 @@
 package com.example.madcamp_androidapp
 
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.madcamp_androidapp.databinding.FragmentGalleryBinding
-import com.example.madcamp_androidapp.ImageDecoration
-import java.io.File
 
 class GalleryFragment : Fragment() {
 
@@ -29,22 +24,40 @@ class GalleryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView = binding.recyclerView
-        // 가로 이미지 개수 설정
-        val spanCount = 3
-        val layoutManager = GridLayoutManager(requireContext(), spanCount)
-        recyclerView.layoutManager = layoutManager
-        // 리사이클러뷰 아이템 간 간격 설정
-        //recyclerView.addItemDecoration(ImageDecoration(100, spanCount))
-        recyclerView.adapter = PhotoAdapter(getPhotoList())
+        val recyclerView1 = binding.recyclerView1
+        val layoutManager1 = LinearLayoutManager(requireContext())
+        val recyclerView2 = binding.recyclerView2
+        val layoutManager2 = LinearLayoutManager(requireContext())
+        val recyclerView3 = binding.recyclerView3
+        val layoutManager3 = LinearLayoutManager(requireContext())
+
+        recyclerView1.layoutManager = layoutManager1
+        recyclerView2.layoutManager = layoutManager2
+        recyclerView3.layoutManager = layoutManager3
+
+        recyclerView1.adapter = PhotoAdapter(getPhotoList1())
+        recyclerView2.adapter = PhotoAdapter(getPhotoList2())
+        recyclerView3.adapter = PhotoAdapter(getPhotoList1())
     }
 
-    private fun getPhotoList(): List<Photo> {
+    private fun getPhotoList1(): List<Photo> {
         val photoList = mutableListOf<Photo>()
         //todo: photoList에 사진 데이터 추가
-        for (i in 1..40) {
+        for (i in 1..20) {
+            photoList.add(Photo("https://ifh.cc/g/Cydxpa.jpg"))
             photoList.add(Photo("https://cdn.pixabay.com/photo/2021/08/03/07/03/orange-6518675_960_720.jpg"))
             photoList.add(Photo("https://ifh.cc/g/zBbY9g.jpg"))
+        }
+        Log.w("check", photoList[0].imageUri)
+        return photoList
+    }
+    private fun getPhotoList2(): List<Photo> {
+        val photoList = mutableListOf<Photo>()
+        //todo: photoList에 사진 데이터 추가
+        for (i in 1..20) {
+            photoList.add(Photo(("https://ifh.cc/g/aLOq4x.jpg")))
+            photoList.add(Photo("https://ifh.cc/g/zBbY9g.jpg"))
+            photoList.add(Photo("https://cdn.pixabay.com/photo/2021/08/03/07/03/orange-6518675_960_720.jpg"))
         }
         Log.w("check", photoList[0].imageUri)
         return photoList
