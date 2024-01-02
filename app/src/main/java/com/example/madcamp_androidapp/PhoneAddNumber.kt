80c0b3636597bfccd0299efdaa898d089b29556b
Kim Sunoh <sunoh1222@gmail.com>
2024-01-02 13:08:57 +0900

    fix: 연락처 추가 탭에서 null값 예외처리 추가
    연락처 추가 탭에서 이름, 전화번호 정보를 모두 입력해야 추가가 가능하도록 수정했습니다. 둘 중 하나라도 기입하지 않았을 경우 추가되지 않도록 처리했습니다.

--- app/src/main/java/com/example/madcamp_androidapp/PhoneAddNumber.kt
+++ app/src/main/java/com/example/madcamp_androidapp/PhoneAddNumber.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.view.View
+import android.widget.Toast
 
 class PhoneAddNumber : AppCompatActivity() {
 
@@ -23,17 +24,24 @@ class PhoneAddNumber : AppCompatActivity() {
 
         // 추가하기 버튼을 눌렀을 때, PhoneFragment로 입력한 이름과 번호를 전달하는 과정
         AddPhoneNum.setOnClickListener {
+            Log.w("name: ", name.toString())
             val newname: String = name.text.toString()
             val newnum: String = num.text.toString()
+            if (!newname.isNullOrEmpty() && !newnum.isNullOrEmpty()) {
+                Log.w("newname: ", newname)
 
-            val resultIntent = Intent() // Intent를 이용해서 전달
-            resultIntent.putExtra("name", newname)
-            resultIntent.putExtra("num", newnum)
-            
-            // Result가 나왔음을 setResult()를 통해 PhoneFragment의 onActivityResult에 전달
-            setResult(Activity.RESULT_OK, resultIntent)
+                val resultIntent = Intent() // Intent를 이용해서 전달
+                resultIntent.putExtra("name", newname)
+                resultIntent.putExtra("num", newnum)
 
-            finish() // Activity 창 닫기
+                // Result가 나왔음을 setResult()를 통해 PhoneFragment의 onActivityResult에 전달
+                setResult(Activity.RESULT_OK, resultIntent)
+
+                finish() // Activity 창 닫기
+            }
+            else {
+                Toast.makeText(this, "이름, 전화번호 정보를 입력하세요.", Toast.LENGTH_SHORT).show()
+            }
         }
 
         // 뒤로 가기 버튼
